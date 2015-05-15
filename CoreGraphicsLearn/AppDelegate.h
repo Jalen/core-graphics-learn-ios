c57034fbc8ba3a0cb2d7d28dad5c78438d59d36b
Jalen Wang <jiayue.wang@autodesk.com>
2015-05-15 10:55:10 +0800

    Initial Commit

---
+++ CoreGraphicsLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreGraphicsLearn
+//
+//  Created by wangjiay on 15/5/15.
+//  Copyright (c) 2015 wangjiay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
